617c066236ac9fb8e551b2812c1c5afbfee412ff
ethanmb7 <ethan.mbn@gmail.com>
2026-07-30 19:57:58 +0200

    Upload manuel de src/components/StatusBar.tsx

---
+++ src/components/StatusBar.tsx
@@ -0,0 +1,12 @@
+export function StatusBar() {
+  return (
+    <div className="status-bar">
+      <span>9:41</span>
+      <div className="status-icons">
+        <span>●●●</span>
+        <span>📶</span>
+        <span>🔋</span>
+      </div>
+    </div>
+  );
+}
